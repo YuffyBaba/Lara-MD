1c6dc11a385f1350f34676137d8435bbdd519ba3
YuffyBaba <lokuyuffy@gmail.com>
2024-12-20 14:42:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "OuYXxKhD#l9ryqrjLK1fG9lvNa6ND-_UaN827gQKpyeUF8nflx0k", // Add your session id
 MONGODB: process.env.MONGODB || "mongodb+srv://laramd:sadeesha2006@anya.gvsp435.mongodb.net/?retryWrites=true&w=majority&appName=Anya", // Add your mongodb url
 };
